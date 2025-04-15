b3029f69cf64e6065bfed261c2bcacf9de90ed5d
Jason Hitching <jasonhitching1994@gmail.com>
2025-04-15 17:10:03 +0100

    Added controller method for all products

--- ProductCatalog/Controllers/ProductsController.cs
+++ ProductCatalog/Controllers/ProductsController.cs
@@ -1,9 +1,30 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
+using ProductCatalog.Models;
+using ProductCatalog.Repositories;
 
 namespace ProductCatalog.Controllers;
 
 public class ProductsController : ControllerBase
 {
+    private readonly ProductRepository repository;
 
+    public ProductsController()
+    {
+        repository = new ProductRepository();
+    }
+
+    [HttpGet]
+    [Route("/products/")]
+    public ActionResult<IEnumerable<Product>> GetProducts()
+    {
+        var products = repository.GetAllProducts();
+
+        if (products is null || !products.Any())
+        {
+            return NotFound("No products were found.");
+        }
+
+        return Ok(products);
+    }
 }
